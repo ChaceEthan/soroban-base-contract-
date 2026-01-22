550662b11646e1c1123e9cab476e8116bb622c2f
ChaceEthan <nzabandoraisiac@gmail.com>
2026-01-22 10:24:59 +0200

    Add Soroban contract with hello function

--- src/libs.rs
+++ src/libs.rs
@@ -1,1 +1,13 @@
+#![no_std]
 
+use soroban_sdk::{contract, contractimpl, Address, Env, Symbol};
+
+#[contract]
+pub struct SorobanContract;
+
+#[contractimpl]
+impl SorobanContract {
+    pub fn hello(env: Env, to: Symbol) -> Symbol {
+        to
+    }
+}
