e9372adf395341c53da59c7691215363e07799cd
ChaceEthan <nzabandoraisiac@gmail.com>
2026-01-22 10:25:42 +0200

    Add unit test for hello function in SorobanContract

--- src/test.rs
+++ src/test.rs
@@ -1,1 +1,14 @@
+#![cfg(test)]
+use super::*;
+use soroban_sdk::{Env, Symbol};
 
+#[test]
+fn test_hello() {
+    let env = Env::default();
+    let contract_id = env.register(SorobanContract, ());
+    let client = SorobanContractClient::new(&env, &contract_id);
+
+    let name = Symbol::from_str(&env, "Dev");
+    let res = client.hello(&name);
+    assert_eq!(res, name);
+}
